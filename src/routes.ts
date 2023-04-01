c13e9fdd273ce7f7d0780bd6b37f3260cefb9997
ismv00 <ismv00@icloud.com>
2023-04-01 00:08:02 -0400

    feat:rota admin criada

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import { prisma } from "./lib/prisma";
+import { json } from "stream/consumers";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post("/admin", async (request, response) => {
+  app.post("/admin", async (request) => {
     const createUser = z.object({
       email: z.string(),
       password: z.string(),
@@ -19,5 +20,27 @@ export async function appRoutes(app: FastifyInstance) {
         password,
       },
     });
+  });
+
+  app.delete("/admin/:id", async (request) => {
+    const id = request.params;
+
+    try {
+      await prisma.user.deleteMany({
+        where: {
+          id: String(id)[0],
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    return "Arquivo deletado com sucesso";
+  });
+
+  app.get("/admin", async (request) => {
+    const users = await prisma.user.findMany();
+
+    return users;
   });
 }
